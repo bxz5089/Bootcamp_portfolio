a21ef96f14e71a709899e6d67cb708c9eea71e28
bxz5089 <69992026+bxz5089@users.noreply.github.com>
2022-03-23 13:21:09 -0400

    Minor updates 3

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const nav = document.getElementById("portfolioNav");
+const tabs = nav.getElementsByClassName("nav-item");
+for (var i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function() {
+  let current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace("active", "");
+  this.className += "active";
+  });
+}
