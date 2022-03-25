6370aac22953e17847f14d5fe242c6f88ff6e9dd
bxz5089 <69992026+bxz5089@users.noreply.github.com>
2022-03-24 21:53:04 -0400

    Minor updates 5

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 const nav = document.getElementById("portfolioNav");
-const tabs = nav.getElementsByClassName("nav-item");
+const tabs = nav.getElementsByClassName("myTab");
+
 for (var i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener("click", function() {
-  let current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace("active", "");
-  this.className += "active";
+  tabs[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+
+    current[0].className = current[0].className.replace("active", "");
+
+    this.className += "active";
   });
 }
